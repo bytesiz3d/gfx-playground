e55f3db878899fc2da332f89bda966010b01c43d
onecalled3bkr <boudy999@gmail.com>
2019-11-18 15:55:14 +0200

    Sweet sweet victory.

--- camera.hpp
+++ camera.hpp
@@ -1,6 +1,15 @@
 #pragma once
 class Camera
 {
+    float yaw;
+    float pitch;
+
+    float yawSens;
+    float pitchSens;
+    float movementSens;
+
+    const float PI;
+
 public:
     glm::vec3 position;
     glm::vec3 direction;
@@ -13,16 +22,40 @@ public:
     float far;
 
     Camera()
+        : yaw(0), pitch(0), yawSens(0.001), pitchSens(0.001), movementSens(0.001), PI(glm::pi<float>())
     {
         position = glm::vec3(0, 0, 0);
         direction = glm::vec3(0, 0, 1);
         up = glm::vec3(0, 1, 0);
 
-        perspectiveFoVy = glm::pi<float>() / 2;
+        perspectiveFoVy = PI / 2;
         aspectRatio = 1;
 
         near = 0.01;
         far = 1000;
+
+        auto dir = direction;
+        auto len = dir[0] * dir[0];
+        len += dir[1] * dir[1];
+        len = std::sqrt(len);
+
+        yaw = std::atan2(dir[2], dir[0]);
+        pitch = std::atan2(dir[1], len);
+    }
+
+    void UpdateCamera(glm::vec2 mouseDelta, glm::vec3 movement)
+    {
+        yaw += mouseDelta[0] * yawSens;
+        pitch += -mouseDelta[1] * pitchSens;
+
+        pitch = glm::min(PI / 2, glm::max(-PI / 2, pitch));
+        direction = glm::vec3(std::cos(yaw) * std::cos(pitch), std::sin(pitch), std::sin(yaw) * std::cos(pitch));
+
+        //movement *= movementSens;
+
+        position += direction * movement[2];
+        position += right() * movement[0];
+        position += up * movement[1];
     }
 
     glm::mat4 ViewMatrix()
@@ -44,6 +77,16 @@ public:
     void SetTarget(glm::vec3 target)
     {
         direction = target - position;
+        direction = glm::normalize(direction);
+
+        // Adjust pitch and yaw:
+        auto dir = direction;
+        auto len = dir[0] * dir[0];
+        len += dir[1] * dir[1];
+        len = std::sqrt(len);
+
+        yaw = std::atan2(dir[2], dir[0]);
+        pitch = std::atan2(dir[1], len);
     }
 
     glm::vec3 right()
--- solar_system.cpp
+++ solar_system.cpp
@@ -12,6 +12,8 @@
 #include <GLFW/glfw3.h>
 
 // GLM
+#include <glm/common.hpp>
+#include <glm/vec2.hpp>
 #include <glm/vec3.hpp>
 #include <glm/mat4x4.hpp>
 #include <glm/ext/matrix_clip_space.hpp>
@@ -19,24 +21,31 @@
 #include <glm/ext/scalar_constants.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-
-#include "solar_system_data.hpp"
 #include "mesh_utils.hpp"
 #include "camera.hpp"
 #include "shader.hpp"
 
+namespace SS
+{
+    #include "solar_system_data.hpp"
 
-bool activeDrawOrbits = true;
-const GLuint WIDTH = 1280, HEIGHT = 720;
-int current_time = 0;
+    bool activeDrawOrbits = true;
+    const GLuint WIDTH = 1280, HEIGHT = 720;
+    int current_time = 0;
 
-void DrawSphere(glm::mat4 MVP, float* tint, Mesh* mesh);
-void DrawOrbit(glm::mat4 MVP, Mesh* mesh);
-void DrawSystem(glm::mat4 parent, SolarSystemDescription system, Mesh* sphereMesh, Mesh* orbitMesh);
+    void DrawSphere(glm::mat4 MVP, float* tint, Mesh* mesh);
+    void DrawOrbit(glm::mat4 MVP, Mesh* mesh);
+    void DrawSystem(glm::mat4 parent, SolarSystemDescription system, Mesh* sphereMesh, Mesh* orbitMesh);
 
-int MVP_location, tint_location;
+    int MVP_location, tint_location;
 
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+    void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+    glm::vec3 movement(0, 0, 0);
+    glm::vec2 mouseDelta(0, 0);
+
+    void mouse_callback(GLFWwindow* window, int button, int action, int mods);
+    bool holdMouse = false;
+};
 
 int main()
 {
@@ -49,7 +58,7 @@ int main()
     glfwWindowHint(GLFW_SAMPLES, 16);
 
     // Create a GLFWwindow object that we can use for GLFW's functions
-    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Solar System", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SS::WIDTH, SS::HEIGHT, "Solar System", NULL, NULL);
     glfwMakeContextCurrent(window);
     if (window == NULL)
     {
@@ -59,7 +68,8 @@ int main()
     }
 
     // Set the required callback functions
-    glfwSetKeyCallback(window, key_callback);
+    glfwSetKeyCallback(window, SS::key_callback);
+    glfwSetMouseButtonCallback(window, SS::mouse_callback);
 
     if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
     {
@@ -68,15 +78,15 @@ int main()
     }
 
     // Define the viewport dimensions
-    glViewport(0, 0, WIDTH, HEIGHT);
+    glViewport(0, 0, SS::WIDTH, SS::HEIGHT);
 
     // Compile and link the shader program:
     GLuint shaderProgram = Shader::LoadShader("shader/color.vert", "shader/color.frag");
     glUseProgram(shaderProgram);
 
     // Get the location of the uniform variables:
-    MVP_location = glGetUniformLocation(shaderProgram, "MVP");
-    tint_location = glGetUniformLocation(shaderProgram, "tint");
+    SS::MVP_location = glGetUniformLocation(shaderProgram, "MVP");
+    SS::tint_location = glGetUniformLocation(shaderProgram, "tint");
 
     // Set the depth test function:
     glEnable(GL_DEPTH_TEST);
@@ -84,10 +94,6 @@ int main()
 
     glClearColor(0.f, 0.f, 0.f, 1.f); // Black background
 
-    GLuint vao;
-    glGenVertexArrays(1, &vao);
-    glBindVertexArray(vao); 
-   
     // Create the meshes:
     Mesh mesh = Mesh_Utils::ColoredSphere();
     Mesh orbitMesh = Mesh_Utils::Orbit();
@@ -96,70 +102,147 @@ int main()
     Camera camera;
     camera.position = glm::vec3(100, 100, 100);
     camera.SetTarget(glm::vec3(0, 0, 0));
-    camera.aspectRatio = (float)WIDTH / HEIGHT;
-    
-    // God forgive me
-    InitSystems();
+    camera.aspectRatio = (float)SS::WIDTH / SS::HEIGHT;
 
+    // #high_level_language
+    SS::InitSystems();
+
+    double p_mX, p_mY,
+           mX,   mY;
+
+    glfwGetCursorPos(window, &p_mX, &p_mY);
     while(!glfwWindowShouldClose(window))
     {
         glfwPollEvents();
-        // TODO: Port the camera controller
+
+        glfwGetCursorPos(window, &mX, &mY);
+        SS::mouseDelta = glm::vec2(mX - p_mX, mY - p_mY);
+        p_mX = mX, p_mY = mY;
+
+        // TODO: Patch up the camera controller (qe, ad, ws)
+        if (SS::holdMouse)
+            camera.UpdateCamera(SS::mouseDelta, SS::movement);
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
         glUseProgram(shaderProgram);
 
-        DrawSystem(camera.ViewProjectionMatrix(), System1, &mesh, &orbitMesh);
-
+        SS::DrawSystem(camera.ViewProjectionMatrix(), SS::System1, &mesh, &orbitMesh);
         glfwSwapBuffers(window);
-        
-        current_time += 5;
+
+        SS::current_time += 5;
     }
     glfwTerminate();
     return false;
 }
 
 // Is called whenever a key is pressed/released via GLFW
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+void SS::key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
 {
     std::cout << key << std::endl;
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, GL_TRUE);
+
+    if (action == GLFW_PRESS)
+    {
+        switch (key)
+        {
+        case GLFW_KEY_W:
+            SS::movement[2] = 1;
+            break;
+
+        case GLFW_KEY_S:
+            SS::movement[2] = -1;
+            break;
+
+        case GLFW_KEY_D:
+            SS::movement[0] = 1;
+            break;
+
+        case GLFW_KEY_A:
+            SS::movement[0] = -1;
+            break;
+
+        case GLFW_KEY_Q:
+            SS::movement[1] = 1;
+            break;
+
+        case GLFW_KEY_E:
+            SS::movement[1] = -1;
+            break;
+
+        default:
+            break;
+        }
+    }
+
+    if (action == GLFW_RELEASE)
+    {
+        switch (key)
+        {
+        case GLFW_KEY_W:
+        case GLFW_KEY_S:
+            SS::movement[2] = 0;
+            break;
+
+        case GLFW_KEY_D:
+        case GLFW_KEY_A:
+            SS::movement[0] = 0;
+            break;
+
+        case GLFW_KEY_Q:
+        case GLFW_KEY_E:
+            SS::movement[1] = 0;
+            break;
+
+        default:
+            break;
+        }
+    }
 }
 
-void DrawSphere(glm::mat4 MVP, float* tint, Mesh* mesh)
+void SS::mouse_callback(GLFWwindow* window, int button, int action, int mods)
 {
-    glUniformMatrix4fv(MVP_location, 1, false, glm::value_ptr(MVP)); // TODO: transpose?
-    glUniformMatrix4fv(tint_location, 1, false, &tint[0]); // TODO: transpose?
+    if (button == GLFW_MOUSE_BUTTON_LEFT)
+    {
+        if (action == GLFW_PRESS)
+            SS::holdMouse = true;
+        else if (action == GLFW_RELEASE)
+            SS::holdMouse = false;
+    }
+}
+
+void SS::DrawSphere(glm::mat4 MVP, float* tint, Mesh* mesh)
+{
+    glUniformMatrix4fv(SS::MVP_location, 1, false, glm::value_ptr(MVP));
+    glUniformMatrix4fv(SS::tint_location, 1, false, &tint[0]);
 
     mesh->draw(GL_TRIANGLES);
 }
 
-void DrawOrbit(glm::mat4 MVP, Mesh* mesh)
+void SS::DrawOrbit(glm::mat4 MVP, Mesh* mesh)
 {
-    glUniformMatrix4fv(MVP_location, 1, false, glm::value_ptr(MVP)); // TODO: transpose?
+    glUniformMatrix4fv(SS::MVP_location, 1, false, glm::value_ptr(MVP));
 
     mesh->draw(GL_LINE_LOOP);
 }
 
-void DrawSystem(glm::mat4 parent, SolarSystemDescription system, Mesh* sphereMesh, Mesh* orbitMesh)
+void SS::DrawSystem(glm::mat4 parent, SolarSystemDescription system, Mesh* sphereMesh, Mesh* orbitMesh)
 {
     glm::mat4 matPlanet = parent;
 
     // Translate and rotate around parent:
-    matPlanet = glm::rotate(matPlanet, current_time * system.rotationSpeedAroundParent, glm::vec3(0, 1, 0));
+    matPlanet = glm::rotate(matPlanet, SS::current_time * system.rotationSpeedAroundParent, glm::vec3(0, 1, 0));
     matPlanet = glm::translate(matPlanet, glm::vec3(system.distanceFromParent, 0, 0));
-    matPlanet = glm::rotate(matPlanet, current_time * system.rotationSpeedAroundParent, glm::vec3(0, 1, 0));
+    matPlanet = glm::rotate(matPlanet, SS::current_time * system.rotationSpeedAroundParent, glm::vec3(0, 1, 0));
 
     glm::mat4 matSystem = matPlanet;
 
     // Rotate around self:
-    matPlanet = glm::rotate(matPlanet, current_time * system.rotationSpeedAroundSelf, glm::vec3(0, 1, 0));
+    matPlanet = glm::rotate(matPlanet, SS::current_time * system.rotationSpeedAroundSelf, glm::vec3(0, 1, 0));
     matPlanet = glm::scale(matPlanet, glm::vec3(system.scale, system.scale, system.scale));
 
     DrawSphere(matPlanet, system.tint, sphereMesh);
-    if (activeDrawOrbits)
+    if (SS::activeDrawOrbits)
     {
         glm::mat4 matOrbit = parent;
         matOrbit = glm::scale(matOrbit, glm::vec3(system.distanceFromParent, system.distanceFromParent, system.distanceFromParent));
